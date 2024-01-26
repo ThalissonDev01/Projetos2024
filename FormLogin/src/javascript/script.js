a367927d8460834b7dc869ed41ed15d6265b7154
Thalisson Costa <141583992+ThalissonDev01@users.noreply.github.com>
2024-01-26 14:35:30 -0300

    projeto button progress

--- FormLogin/src/javascript/script.js
+++ FormLogin/src/javascript/script.js
@@ -11,3 +11,89 @@ signInLink.addEventListener('click', () => {
     wrapper.classList.add('animate-signUp');
     wrapper.classList.remove('animate-signIn');
 });
+
+// Validação de formulario
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    checkForm();
+})
+
+username.addEventListener('blur', () => {
+    checkInputUsername();
+})
+
+email.addEventListener('blur', () => {
+    checkInputEmail();
+})
+
+password.addEventListener('blur', () => {
+    checkInputPassword();
+})
+
+
+function checkInputUsername() {
+    const usernameValue = username.value;
+
+    if (usernameValue === '') {
+        errorInput(username, 'Preencha o seu nome!')
+    } else {
+        const formItem = username.parentElement;
+        formItem.classList = 'input-group'
+    }
+}
+
+function checkInputEmail() {
+    const emailValue = email.value;
+
+    if (emailValue === '') {
+        errorInput(email, 'Preencha o email!')
+    } else {
+        const formItem = email.parentElement;
+        formItem.className = 'input-group'
+    }
+}
+
+function checkInputPassword() {
+    const passwordValue = password.value;
+
+    if (passwordValue === '') {
+        errorInput(password, 'Preencha a senha!')
+    } else if (passwordValue.length < 8) {
+        errorInput(password, 'No minimmo 8 caracteres.')
+    } else {
+        const formItem = password.parentElement;
+        formItem.className = 'input-group' 
+    }
+}
+
+function checkForm() {
+    checkInputUsername();
+    checkInputEmail();
+    checkInputPassword();
+
+    const formItem = form.querySelectorAll('.input-group')
+
+    const isValid = [...formItem].every( (item) => {
+        return item.className === 'input-group'
+    });
+
+    if (isValid) {
+        alert('Cadastrado com sucesso!')
+    }
+}
+
+function errorInput(input, message) {
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector('a')
+
+    textMessage.innerText = message;
+
+    formItem.className = 'input-group error'
+}
